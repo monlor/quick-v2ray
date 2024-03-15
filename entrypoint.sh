7f35ff0ad7251eb0708e66594e763ccd509b22da
monlor <me@monlor.com>
2024-03-15 20:43:13 +0800

    fix: 🐛 修复caddy配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-DOMAIN="${DOMAIN}"
+#!/bin/sh
+DOMAIN="${DOMAIN:-example.com}"
 UUID="${UUID:-$(cat /proc/sys/kernel/random/uuid)}"
 ALTER_ID="${ALTER_ID:-64}"
 PROXY_PATH="${PROXY_PATH:-/}"
@@ -7,12 +7,15 @@ PROXY_PORT="${PROXY_PORT:-80}"
 
 echo "生成Caddyfile..."
 cat > /etc/Caddyfile <<-EOF
-${DOMAIN} {
-  log ./caddy.log
-  proxy ${PROXY_PATH} :${PROXY_PORT} {
-    websocket
-    header_upstream -Origin
-  }
+http://${DOMAIN}:${PROXY_PORT}  {
+    @websockets {
+        header Connection *Upgrade*
+        header Upgrade websocket
+        
+    }
+    reverse_proxy @websockets ${PROXY_PATH} {
+        header_up -Origin
+    }
 }
 EOF
 
@@ -20,7 +23,7 @@ echo "启动caddy..."
 caddy start --config /etc/Caddyfile --adapter caddyfile
 
 echo "生成v2ray配置..."
-cat > /etc/v2ray/config.json <<-EOF
+cat > /etc/v2ray.json <<-EOF
 {
   "inbounds": [
     {
@@ -54,4 +57,4 @@ EOF
 echo "v2ray url: vmess://$(echo auto:${UUID}@${DOMAIN}:${PROXY_PORT} | base64)?remarks=${DOMAIN}&obfsParam=www.microsoft.com&path=/&obfs=websocket&tfo=1&alterId=${ALTER_ID}"
 
 echo "启动v2ray..."
-/usr/bin/v2ray -config /etc/v2ray/config.json
+/usr/bin/v2ray -config /etc/v2ray.json
